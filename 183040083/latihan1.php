807fed98edac2ed074ec7ca2078ee6498f18c25d
Ridha Ahmad Firdaus <ridhaaf@gmail.com>
2020-11-19 22:20:07 +0700

    Initial commit

---
+++ 183040083/latihan1.php
@@ -0,0 +1,17 @@
+<?php
+
+$mahasiswa = [
+    [
+        "nama" => "Ridha Ahmad Firdaus",
+        "nrp" => "183040083",
+        "email" => "ridhaaf@gmail.com"
+    ],
+    [
+        "nama" => "Ridha Ahmad Firdaus",
+        "nrp" => "183040083",
+        "email" => "ridhaaf@gmail.com"
+    ]
+];
+
+$data = json_encode($mahasiswa);
+echo $data;
---
+++ 183040083/script.js
@@ -0,0 +1,7 @@
+let mahasiswa = {
+    nama : "Ridha Ahmad Firdaus",
+    nrp : "183040083",
+    email: " ridhaaf@gmail.com"
+}
+
+console.log(JSON.stringify(mahasiswa));
